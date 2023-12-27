45587a654550eb191f5090b89a3639900bf2ab15
jine2 <139872569+jine2@users.noreply.github.com>
2023-12-27 16:43:53 +0900

    12/27 리팩터링 전 코드 작성
    12/27 리팩터링 전 코드 작성

---
+++ CodingTest/src/baekjoon/quiz1157/OriginalCode.java
@@ -0,0 +1,39 @@
+package baekjoon.quiz1157;
+
+import java.util.Scanner;
+
+public class OriginalCode {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        word = word.toUpperCase();
+        char[] array = new char[26];
+
+        for (int i = 0; i < word.length(); i++) {
+            array[word.charAt(i) - 65] += 1;
+        }
+
+        int max = 0;
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+                index = i;
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == max) {
+                count++;
+            }
+        }
+
+        if (count == 1) {
+            System.out.println((char) (index + 65));
+        } else {
+            System.out.println("?");
+        }
+
+    }
+}
