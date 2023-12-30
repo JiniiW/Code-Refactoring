251efb5367141616b788762e2905309ea3253c2a
jine2 <139872569+jine2@users.noreply.github.com>
2023-12-31 02:08:19 +0900

    리팩터링 전 코드 작성
    리팩터링 전 코드 작성

---
+++ ClassNotes/src/polymorphisomOfParameters/originalcode/Computer.java
@@ -0,0 +1,10 @@
+package polymorphisomOfParameters.originalcode;
+
+public class Computer extends Product{
+    Computer(){
+        super(50);
+    }
+    public String toString(){
+        return "Computer";
+    }
+}
---
+++ ClassNotes/src/polymorphisomOfParameters/originalcode/TV.java
@@ -0,0 +1,10 @@
+package polymorphisomOfParameters.originalcode;
+
+public class TV extends Product{
+    TV(){
+        super(100);
+    }
+    public String toString(){
+        return "TV";
+    }
+}
